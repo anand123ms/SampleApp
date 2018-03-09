11133843bcec11e707b1622d3cced56eb1ee59fc
Admin <admin@ntinltp27003195.local>
2018-03-09 14:47:01 +0530

    Changes made in ViewController class

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -14,11 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-<<<<<<< HEAD
-      //Working on feature branch
-=======
         //Changes is made Dev  branch
->>>>>>> Dev
     }
 
     override func didReceiveMemoryWarning() {
